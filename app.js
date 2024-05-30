96b36abe5bc5ee1bfc7ad146ee42f3eead2d13fd
Satish <satishpati96548@gmail.com>
2024-05-30 11:11:02 +0530

    first commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ async function main() {
     await mongoose.connect(mongo_url)
 }
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
